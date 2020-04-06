75659c6ab4d43323ca2679f44c6f430cf3e9c001
Adao Natalino <sansonov@gmail.com>
2020-04-06 17:36:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,11 @@
 require 'pry'
 def run_guessing_game
-  randon = rand(6) + 1
+  randon = (rand(6) + 1).to_s
   puts 'Guess your number between 1 and 6!'
   user_number = gets.chomp
-  array = [1, 2, 3, 4, 5, 6]
+  array = ["1", "2", "3", "4", "5", "6"]
   array.delete(randon)
+  binding.pry
   if user_number == randon
     puts "You guessed the correct number!"
     #binding.pry
@@ -12,7 +13,7 @@ def run_guessing_game
     #binding.pry
       puts "Sorry! The computer guessed #{randon}."
     elsif user_number == 'exit'
-    binding.pry
+    #binding.pry
       puts "Goodbye!"
   end
 end

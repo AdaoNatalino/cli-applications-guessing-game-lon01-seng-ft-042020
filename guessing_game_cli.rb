2a0f1f05c76bc51ab26f98d0108e972015ae3c53
Adao Natalino <sansonov@gmail.com>
2020-04-06 17:29:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,10 +5,10 @@ def run_guessing_game
   user_number = gets.chomp
   array = [1, 2, 3, 4, 5, 6]
   array.delete(randon)
-  #binding.pry
   if user_number == randon
     puts "You guessed the correct number!"
     elsif array.include?(user_number)
+    binding.pry
       puts "Sorry! The computer guessed #{randon}."
     elsif user_number == 'exit'
       puts "Goodbye!"
